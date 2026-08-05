713bb20b09d395d701d446243b958833aa329941
gmlgskt131-pixel <gemilangsakti31@gmail.com>
2026-08-06 06:01:07 +0700

    Update fetch_loker.py

--- fetch_loker.py
+++ fetch_loker.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+IKLAN = "https://crypotential.com/kxseizepn?key=b27dbc018fb141e5773a6cc85f207c78"
 RSS_URL = "https://rss.app/feeds/MZMZkLtTiHKbr2ck.xml"
 
 xml = requests.get(RSS_URL, timeout=30).text
@@ -25,9 +26,16 @@ for item in items[:5]:
 
 🏢 {title}
 
-📝 {desc[:300]}
+📝 {desc[:250]}
 
-🔗 {link}
+🔗 Link Lowongan:
+{link}
+
+━━━━━━━━━━━━━━
+
+💰 Ingin mendapatkan penghasilan tambahan dari HP?
+
+👉 {IKLAN}
 
 ━━━━━━━━━━━━━━
 🤖 INFO LOKER CIAYUMAJAKUNING
