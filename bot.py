b489093426b2df5a297b42be1a13a339a1c9f0e7
gmlgskt131-pixel <gemilangsakti31@gmail.com>
2026-08-07 01:00:37 +0700

    Implement basic Telegram bot with commands

---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+import telebot
+
+BOT_TOKEN = os.getenv("8883789126:AAFs-YoNg2oBvtkezZAcveq-1qmBMrDIkIE")
+
+bot = telebot.TeleBot(8883789126:AAFs-YoNg2oBvtkezZAcveq-1qmBMrDIkIE)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(
+        message,
+        "👋 Selamat datang di Bot Info Loker Ciayumajakuning!\n\n"
+        "Ketik /help untuk melihat menu."
+    )
+
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+    bot.reply_to(
+        message,
+        "/start - Mulai bot\n"
+        "/help - Bantuan\n"
+        "/loker - Lowongan terbaru"
+    )
+
+@bot.message_handler(commands=['loker'])
+def loker(message):
+    bot.reply_to(
+        message,
+        "📢 Fitur lowongan sedang disiapkan."
+    )
+
+print("Bot aktif...")
+bot.infinity_polling(skip_pending=True)
