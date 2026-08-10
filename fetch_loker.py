02e724e4ac3598f1251d0d2e3da7756bad456197
gmlgskt131-pixel <gemilangsakti31@gmail.com>
2026-08-10 17:48:06 +0700

    Update CHAT_ID environment variable

--- fetch_loker.py
+++ fetch_loker.py
@@ -9,7 +9,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8861664027:AAFhS__mhGD07rvyZ3Oyr8re0Tau8_lKw3w")
-CHAT_ID = os.getenv("CHAT_ID", "8684396228")
+CHAT_ID = os.getenv("CHAT_ID", "-1004426208468")
 
 # Masukkan daftar RSS yang aktif (URL 404 bisa dihapus atau diperbarui)
 RSS_FEEDS = [
